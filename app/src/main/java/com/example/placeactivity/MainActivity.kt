f2fe5eff293d265c90ca177b30d5870b58b027ad
vjilka852 <vjilka852@rku.ac.in>
2026-02-23 09:58:39 +0530

    Second Activity

--- app/src/main/java/com/example/placeactivity/MainActivity.kt
+++ app/src/main/java/com/example/placeactivity/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.placeactivity
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        Handler().postDelayed(
+            {
+                var intent = Intent(this,secondActivity::class.java)
+                startActivity(intent)
+            },5000
+        )
+
     }
 }
